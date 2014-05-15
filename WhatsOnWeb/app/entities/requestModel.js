0197a96ca2c6e0a3f8287b5ee603ae0d3158376f
Toby <tobysevans@gmail.com>
2014-05-15 13:52:01 +0100

    Added requestModel, guaranteed to have valid values

---
+++ WhatsOnWeb/app/entities/requestModel.js
@@ -0,0 +1,20 @@
+﻿var requestModel = function (params) {
+    
+    var that = {};
+    var values = {};
+    var checkedParams = params || {};
+    values.letter = checkedParams.letter || "a";
+    values.page = getNumberFromParam(checkedParams.page, 1); //could do number check here
+    values.size = getNumberFromParam(checkedParams.size , 20); //could do number check here
+
+    function getNumberFromParam(value, defaultValue) {
+        if (isNaN(value)) return defaultValue;
+        return value || defaultValue;
+    }
+
+  
+    that.letter = function() { return values.letter; };
+    that.page = function() { return values.page; };
+    that.size = function() { return values.size; };
+    return that;
+}
---
+++ WhatsOnWeb/app/tests/entityTests.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../../lib/jasmine/jasmine.js" />
+/// <reference path="../entities/requestModel.js" />
+
+
+describe("Request model", function () {
+    describe("when not supplied parameters, should default to", function () {
+        var thisRequestModel = requestModel();
+        it("letter 'a'", function() {
+            expect(thisRequestModel.letter()).toBe("a");
+        });
+        it("page 1", function () {
+            expect(thisRequestModel.page()).toBe(1);
+        });
+        it("size 20", function () {
+            expect(thisRequestModel.size()).toBe(20);
+        });
+    });
+
+    describe("when supplied parameters 'letter: 'b', page:5, size:15'", function () {
+        var params = { letter: "b", page:5, size:15 };
+        var thisRequestModel = requestModel(params);
+        it("letter 'b'", function () {
+            expect(thisRequestModel.letter()).toBe("b");
+        });
+        it("page 5", function () {
+            expect(thisRequestModel.page()).toBe(5);
+        });
+        it("size 15", function () {
+            expect(thisRequestModel.size()).toBe(15);
+        });
+    });
+
+    describe("when supplied non-number page and size values", function() {
+        var params = {  page: "five", size: "size" };
+        var thisRequestModel = requestModel(params);
+        it("default to page 1", function () {
+            expect(thisRequestModel.page()).toBe(1);
+        });
+        it("default to size 20", function () {
+            expect(thisRequestModel.size()).toBe(20);
+        });
+    });
+});
