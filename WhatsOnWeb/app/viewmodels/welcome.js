e731bb06d5dcc9ba42663f72fbac7697952223fb
Toby <tobysevans@gmail.com>
2014-05-16 13:35:42 +0100

    Added explanatory notes

--- WhatsOnWeb/app/viewmodels/welcome.js
+++ WhatsOnWeb/app/viewmodels/welcome.js
@@ -1,26 +1,22 @@
 ﻿define(function() {
     var ctor = function () {
         this.displayName = 'What\'s on';
-        this.description = 'Find out when your favourite programmes are on!';
+        this.description = 'Sample programme guide for BBC TV content!';
         this.features = [
-            'Clean MV* Architecture',
-            'JS & HTML Modularity',
-            'Simple App Lifecycle',
-            'Eventing, Modals, Message Boxes, etc.',
-            'Navigation & Screen State Management',
-            'Consistent Async Programming w/ Promises',
-            'App Bundling and Optimization',
-            'Use any Backend Technology',
-            'Built on top of jQuery, Knockout & RequireJS',
-            'Integrates with other libraries such as SammyJS & Bootstrap',
-            'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
+            'fully unit tested, with external dependencies mocked',
+            'all abstract domain data types, with no dependencies on external domain',
+            'SOLID principles observed throughout',
+            'pluggable API data sources',
+            'pluggable HTTP request broker, currently JQuery but simple to switch ',
+            'no application dependency on UI',
+            'Durandal/knockout used for presentation, easily switched to any other',
+            'requirejs used to map domain objects/services to presentation layer',
+            'uses static image size, can potentially make dynamic',
+            'simple error handling if request fails'
         ];
     };
 
-    //Note: This module exports a function. That means that you, the developer, can create multiple instances.
-    //This pattern is also recognized by Durandal so that it can create instances on demand.
-    //If you wish to create a singleton, you should export an object instead of a function.
-    //See the "flickr" module for an example of object export.
+    
 
     return ctor;
 });
